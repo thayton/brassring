381ef2976442318f8597112a67cb2eaa3fe3109e
Matthew Todd Hayton <mhayton@Todds-MBP.home>
2015-01-15 09:26:43 -0500

    updates

--- brassring.py
+++ brassring.py
@@ -13,22 +13,29 @@ class BrassringJobScraper(object):
         self.numJobsSeen = 0
 
     def open_search_openings_page(self):
+        r = re.compile(r'Search openings', re.I)
         self.br.open(self.url)
-        self.br.follow_link(self.br.find_link(text_regex=re.compile(r'Search openings', re.I)))
+        self.br.follow_link(self.br.find_link(text_regex=r))
         
     def soupify_form(self, soup, form_name):
-        #
-        # Some of script contents throw off mechanize
-        # and it gives error 'ParseError: OPTION outside of SELECT'
-        # So we soupify it to remove script contents
-        #
+        '''
+        Selecting a form with mechanize sometimes throws the error
+
+        'ParseError: OPTION outside of SELECT'
+
+        Running the form through BeautifulSoup seems to fix the issue
+        '''
         if not soup:
             soup = BeautifulSoup(self.br.response().read())
 
         form = soup.find('form', attrs={'name': form_name})
         html = str(form)
-        resp = mechanize.make_response(html, [("Content-Type", "text/html")],
-                                       self.br.geturl(), 200, "OK")
+        resp = mechanize.make_response(
+            html, 
+            [("Content-Type", "text/html")],
+            self.br.geturl(),
+            200, "OK"
+        )
         self.br.set_response(resp)
 
     def submit_search_form(self):
@@ -50,20 +57,20 @@ class BrassringJobScraper(object):
         self.soup = BeautifulSoup(self.br.response().read())
 
     def scrape_jobs(self):
-        r = re.compile(r'^jobdetails\.asp')
-
         while not self.seen_all_jobs():
-            i = self.soup.find('input', id='ctl00_MainContent_GridFormatter_json_tabledata')
+            t = 'ctl00_MainContent_GridFormatter_json_tabledata'
+            i = self.soup.find('input', id=t)
             j = json.loads(i['value'])
 
             for x in j:
                 # debug
                 print '\n'.join('%s\t%s' % (y,z) for y,z in x.items())
+                print '\n'
 
                 job = {}
-                job['title'] = self.get_title_from_dict(x)
-                job['location'] = self.get_location_from_dict(x)
-                job['url'] = self.get_url_from_dict(x)
+                job['title'] = self.get_title_from_job_dict(x)
+                job['location'] = self.get_location_from_job_dict(x)
+                job['url'] = self.get_url_from_job_dict(x)
 
                 self.jobs.append(job)
 
@@ -72,17 +79,17 @@ class BrassringJobScraper(object):
             # Next page
             self.goto_next_page()
 
-    def get_title_from_dict(self, dict):
+    def get_title_from_job_dict(self, job_dict):
         pass
 
-    def get_location_from_dict(self, dict):
+    def get_location_from_job_dict(self, job_dict):
         pass
 
-    def get_soup_anchor_from_dict(self, dict):
+    def get_soup_anchor_from_job_dict(self, job_dict):
         pass
 
-    def get_url_from_dict(self, dict):
-        a = self.get_soup_anchor_from_dict(dict)
+    def get_url_from_job_dict(self, job_dict):
+        a = self.get_soup_anchor_from_job_dict(job_dict)
         u = urlparse.urljoin(self.br.geturl(), a['href'])
         u = self.refine_url(u)
         return u
@@ -90,7 +97,10 @@ class BrassringJobScraper(object):
     def refine_url(self, job_url):
         """
         """
-        items = urlutil.url_query_get(self.url.lower(), ['partnerid', 'siteid'])
+        items = urlutil.url_query_get(
+            self.url.lower(), 
+            ['partnerid', 'siteid']
+        )
 
         url = urlutil.url_query_filter(job_url, 'jobId')
         url = urlutil.url_query_add(url, items.iteritems())
